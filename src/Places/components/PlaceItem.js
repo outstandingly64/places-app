3030529050e38c8a4f032214a848613732bf24e6
Ivan <ivantello2013@gmail.com>
2022-09-11 13:14:08 -0400

    Pre-Map Feature

--- src/Places/components/PlaceItem.js
+++ src/Places/components/PlaceItem.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../../Shared/components/UIElements/Card";
+import Button from "../../Shared/components/FormElements/Button";
+import Modal from "../../Shared/components/UIElements/Modal";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
-        </div> 
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+            <h2>TODO: INSERT MAP HERE</h2>
         </div>
-        <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
