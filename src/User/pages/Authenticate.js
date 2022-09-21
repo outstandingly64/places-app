6d04c13a530dfa3351bac1e6f64237687459eaa7
Ivan Tello <ivantello9922@gmail.com>
2022-09-21 16:22:40 -0400

    Switch between Login/Signup State

--- src/User/pages/Authenticate.js
+++ src/User/pages/Authenticate.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import './Authenticate.css';
-import Card from '../../Shared/components/UIElements/Card';
-import Input from '../../Shared/components/FormElements/Input';
-import Button from '../../Shared/components/FormElements/Button';
+import React, { useState } from "react";
+import "./Authenticate.css";
+import Card from "../../Shared/components/UIElements/Card";
+import Input from "../../Shared/components/FormElements/Input";
+import Button from "../../Shared/components/FormElements/Button";
 
-import {useForm} from "../../Shared/Hooks/form-hook";
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../Shared/Utilities/validators';
+import { useForm } from "../../Shared/Hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../Shared/Utilities/validators";
 
 const Authenticate = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   const initialInputs = {
     email: {
       value: "",
@@ -17,43 +23,77 @@ const Authenticate = () => {
       value: "",
       isValid: false,
     },
-
   };
 
- const [formState, inputHandler] = useForm(initialInputs, false);
+  const [formState, inputHandler, setFormData] = useForm(initialInputs, false);
 
- const authSubmitHandler = event => {
-  event.preventDefault();
-  console.log(formState.inputs);
- };
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  const switchModeHandler = () => {
+    if(!loggedIn){
+        setFormData({
+        ...formState.inputs,
+          name: undefined,
+        }, formState.inputs.email.isValid && formState.inputs.password.isValid);
+    }else{
+      setFormData({
+        ...formState.inputs,
+        name: {
+          value: '',
+          isValid: false
+        }
+      }, false);
+    }
+
+    setLoggedIn((prevMode) => !prevMode);
+  };
 
   return (
     <Card className="authentication">
       <h2>Login Required</h2>
-      <hr/>
+      <hr />
       <form onSubmit={authSubmitHandler}>
+        {!loggedIn && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Fullname"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a royal name!"
+            onInput={inputHandler}
+          />
+        )}
         <Input
-        id="email"
-        element="input"
-        type="email"
-        label="E-mail"
-        validators={[VALIDATOR_EMAIL()]}
-        errorText="Please enter valid email address."
-        onInput={inputHandler}
+          id="email"
+          element="input"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter valid email address."
+          onInput={inputHandler}
         />
         <Input
-        id="password"
-        element="input"
-        type="password"
-        label="password"
-        validators={[VALIDATOR_MINLENGTH(7)]}
-        errorText="Please enter valid password, at least 7 characters."
-        onInput={inputHandler}
+          id="password"
+          element="input"
+          type="password"
+          label="password"
+          validators={[VALIDATOR_MINLENGTH(7)]}
+          errorText="Please enter valid password, at least 7 characters."
+          onInput={inputHandler}
         />
-        <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
+        <Button type="submit" disabled={!formState.isValid}>
+          {loggedIn ? "LOGIN" : "SIGNUP"}
+        </Button>
       </form>
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {loggedIn ? "SIGNUP" : "LOGIN"}
+      </Button>
     </Card>
-  )
-}
+  );
+};
 
 export default Authenticate;
