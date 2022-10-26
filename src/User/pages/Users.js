e07130bc1ef207f2f961fd1ec8ad2d605929e031
Ivan Tello <ivantello9922@gmail.com>
2022-10-26 18:34:22 -0400

    fetching users refactor: useHttpClient in action

--- src/User/pages/Users.js
+++ src/User/pages/Users.js
@@ -2,53 +2,36 @@ import React, { useEffect, useState } from "react";
 import UserList from "../components/UsersList";
 import ErrorModal from "../../Shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../Shared/components/UIElements/LoadingSpinner";
-
-//TODO: Refactor and create a custom http request hook for this component
+import { useHttpClient } from "../../Shared/Hooks/http-hook";
 
 /**
  * we want to send a GET request of the users
  * whenever THIS page loads.
  */
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const {isLoading, error, sendRequest, clearError} = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
 
   // useEffect to prevent infinite loop of fetch request during re-renders
   // NOTE: THE DEFUALT REQUEST TYPE OF FETCH() IS GET
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUsers = async () => {
 
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-
-        // response should be an object with a 'users' key
-        // which is an array of users
-        const responseData = await response.json();
-
-        if(!response.ok){
-          throw new Error(responseData.message);
-        }
+        // only needs url in this case: httpHook has default parameters for GET requests 
+        const responseData = await sendRequest("http://localhost:5000/api/users");
 
         setLoadedUsers(responseData.users);
-        setIsLoading(false);
       } catch (err) {
-        setError(err.message);
       }
-      setIsLoading(false);
     };
 
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  }
+    fetchUsers();
+  }, [sendRequest]);
 
   return(
     <>
-    <ErrorModal error={error} onClear={errorHandler}/>
+    <ErrorModal error={error} onClear={clearError}/>
     {isLoading && (
       <div className="center">
         <LoadingSpinner/>
