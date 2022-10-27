01f3edc743c9d419d2908c330942f74bd545946c
Ivan Tello <ivantello9922@gmail.com>
2022-10-27 10:11:11 -0400

    getting places by user id via frontend

--- src/Places/components/PlaceList.js
+++ src/Places/components/PlaceList.js
@@ -31,7 +31,7 @@ const PlaceList = (props) => {
         <PlaceItem 
         key={place.id}
         id={place.id}
-        image={place.imageUrl}
+        image={place.image}
         title={place.title}
         description={place.description}
         address={place.address}
--- src/Places/pages/UserPlaces.js
+++ src/Places/pages/UserPlaces.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { DUMMY_PLACES } from '../../Shared/DUMMY/dummy-objects';
 
 import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../Shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../Shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../Shared/Hooks/http-hook";
 
 const UserPlaces = (props) => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
-  //TODO: HOOK UP TO BACKEND
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+  
+  return (
+    <>
+    <ErrorModal error={error} onClear={clearError}/>
+    {isLoading && <div className="center"><LoadingSpinner/></div>}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 export default UserPlaces;
