da99d08350503aacfba3c2a1c4a957aff25597f5
Ivan Tello <ivantello9922@gmail.com>
2022-11-03 17:55:40 -0400

    minor fix: place creator id assigned in backend

--- src/Places/pages/NewPlace.js
+++ src/Places/pages/NewPlace.js
@@ -50,7 +50,6 @@ const NewPlace = () => {
       formData.append('title', formState.inputs.title.value);
       formData.append('description', formState.inputs.description.value);
       formData.append('address', formState.inputs.address.value);
-      formData.append('creator', auth.userId);
       formData.append('image', formState.inputs.image.value);
       await sendRequest(
         "http://localhost:5000/api/places",
