b975b689abee0839a676157f184ea3971ce2e5de
Ivan Tello <ivantello9922@gmail.com>
2022-11-19 15:52:40 -0500

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,13 +7,20 @@ import {
 } from "react-router-dom";
 
 import MainNavigation from "./Shared/components/Navigation/MainNavigation";
-import NewPlace from "./Places/pages/NewPlace";
-import UpdatePlace from "./Places/pages/UpdatePlace";
-import UserPlaces from "./Places/pages/UserPlaces";
-import Users from "./User/pages/Users";
-import Authenticate from "./User/pages/Authenticate";
+import LoadingSpinner from "./Shared/components/UIElements/LoadingSpinner";
+//import Users from "./User/pages/Users";
+//import UserPlaces from "./Places/pages/UserPlaces";
+//import NewPlace from "./Places/pages/NewPlace";
+//import UpdatePlace from "./Places/pages/UpdatePlace";
+//import Authenticate from "./User/pages/Authenticate";
 import { AuthContext } from "./Shared/context/auth-context";
 import { useAuth } from "./Shared/Hooks/auth-hook";
+
+const Users = React.lazy(() => import("./User/pages/Users"));
+const UserPlaces = React.lazy(() => import("./Places/pages/UserPlaces"));
+const NewPlace = React.lazy(() => import("./Places/pages/NewPlace"));
+const UpdatePlace = React.lazy(() => import("./Places/pages/UpdatePlace"));
+const Authenticate = React.lazy(() => import("./User/pages/Authenticate"));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -59,7 +66,17 @@ const App = () => {
       <Router>
         <MainNavigation />
         <main>
-          <Routes>{routes}</Routes>
+            <Suspense
+              fallback={
+                <div className="center">
+                    <LoadingSpinner />
+                  </div>
+              }
+              >
+              <Routes>
+              {routes}
+          </Routes>
+            </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
